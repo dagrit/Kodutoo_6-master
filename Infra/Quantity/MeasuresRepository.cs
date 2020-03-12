1b2009683a86c1c12228873706bb5619b5074108
Daniil Gritsak <dagrit@ttu.ee>
2020-03-12 23:33:00 +0200

    Add project files.

---
+++ Infra/Quantity/MeasuresRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
+
+namespace Abc.Infra.Quantity
+{
+    public class MeasuresRepository : UniqueEntityRepository<Measure, MeasureData>, IMeasuresRepository
+    {
+        public MeasuresRepository(QuantityDbContext c) : base(c, c.Measures) { }
+
+        protected internal override Measure toDomainObjects(MeasureData d) =>new Measure(d);
+
+    }
+}
---
+++ Tests/SealedClassTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests
+{
+    public abstract class SealedClassTest<TClass, TBaseClass> : ClassTest<TClass, TBaseClass> where TClass: new()
+    {
+        [TestMethod]
+        public void IsSealed()
+        {
+            Assert.IsTrue(type.IsSealed);
+        }
+    }
+}
